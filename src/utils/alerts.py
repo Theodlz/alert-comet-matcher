1da4279bce3dc896906ca5e951495b2b45d558e5
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-07-10 16:57:56 -0400

    Refactor query generation and result aggregation in alerts.py for improved clarity and efficiency.

--- src/utils/alerts.py
+++ src/utils/alerts.py
@@ -29,104 +29,58 @@ def bulk_query_moving_objects(
 
     # reformat to have a dict with keys as epochs and values as lists of objects and their positions at that epoch
     epochs = tuple(objects_with_positions[list(objects_with_positions.keys())[0]]["jd"])
-    if max_queries_per_batch is None:
-        queries = []
-        for i, epoch in tqdm(
-            enumerate(epochs),
-            total=len(epochs),
-            desc="Generating queries",
-            disable=not verbose,
-        ):
-            objects = {}
-            catalog_parameters = {
-                stream: {
-                    "filter": {
-                        "candidate.jd": {"$gte": epoch - 0.01, "$lte": epoch + 0.01}
-                    },
-                    "projection": {
-                        "_id": 0,
-                        "candid": 1,
-                        "objectId": 1,
-                        "candidate.jd": 1,
-                    },
-                }
-            }
-            for obj_name in objects_with_positions:
-                objects[obj_name] = [
-                    objects_with_positions[obj_name]["ra"][i],
-                    objects_with_positions[obj_name]["dec"][i],
-                ]
-            queries.append(
-                build_cone_search(
-                    objects, catalog_parameters, radius=5.0, unit="arcsec"
-                )
-            )
-        results = run_queries(
-            k, queries=queries, query_type="cone_search", n_processes=n_processes
-        )
 
-        return results[stream]
-    else:
-        all_results = {}
-        max_queries_per_batch = min(int(max_queries_per_batch), len(epochs))
-        n_batches = int(len(epochs) / max_queries_per_batch)
-        with tqdm(total=n_batches * max_queries_per_batch, disable=not verbose) as pbar:
-            for i in range(n_batches):
-                queries = []
-                for j, epoch in enumerate(
-                    epochs[
-                        i
-                        * max_queries_per_batch : (i + 1)  # noqa E203
-                        * max_queries_per_batch
+    max_queries_per_batch = min(max_queries_per_batch, len(epochs)) if max_queries_per_batch else len(epochs)
+    n_batches = int(len(epochs) / max_queries_per_batch)
+
+    all_results = {}
+    with tqdm(total=n_batches * max_queries_per_batch, disable=not verbose) as pbar:
+        for i in range(n_batches):
+            queries = []
+            for j, epoch in enumerate(
+                    epochs[i * max_queries_per_batch: (i + 1) * max_queries_per_batch]
+            ):
+                objects = {
+                    obj_name: [
+                        objects_with_positions[obj_name]["ra"][i * max_queries_per_batch + j],
+                        objects_with_positions[obj_name]["dec"][i * max_queries_per_batch + j],
                     ]
-                ):
-                    objects = {}
-                    catalog_parameters = {
-                        stream: {
-                            "filter": {
-                                "candidate.jd": {
-                                    "$gte": epoch - 0.01,
-                                    "$lte": epoch + 0.01,
-                                }
-                            },
-                            "projection": {
-                                "_id": 0,
-                                "candid": 1,
-                                "objectId": 1,
-                                "candidate.jd": 1,
-                            },
-                        }
+                    for obj_name in objects_with_positions
+                }
+
+                catalog_parameters = {
+                    stream: {
+                        "filter": {
+                            "candidate.jd": {
+                                "$gte": epoch - 0.01,
+                                "$lte": epoch + 0.01,
+                            }
+                        },
+                        "projection": {
+                            "_id": 0,
+                            "candid": 1,
+                            "objectId": 1,
+                            "candidate.jd": 1,
+                        },
                     }
-                    for obj_name in objects_with_positions:
-                        objects[obj_name] = [
-                            objects_with_positions[obj_name]["ra"][
-                                i * max_queries_per_batch + j
-                            ],
-                            objects_with_positions[obj_name]["dec"][
-                                i * max_queries_per_batch + j
-                            ],
-                        ]
-                    queries.append(
-                        build_cone_search(
-                            objects, catalog_parameters, radius=5.0, unit="arcsec"
-                        )
+                }
+
+                queries.append(
+                    build_cone_search(
+                        objects, catalog_parameters, radius=5.0, unit="arcsec"
                     )
-                # run the queries
-                results = run_queries(
-                    k,
-                    queries=queries,
-                    query_type="cone_search",
-                    n_processes=n_processes,
                 )
 
-                if i == 0:
-                    all_results = results[stream]
-                else:
-                    for obj_name in all_results:
-                        all_results[obj_name] += results[stream][
-                            obj_name
-                        ]
+            results = run_queries(
+                k,
+                queries=queries,
+                query_type="cone_search",
+                n_processes=n_processes,
+            )
 
-                pbar.update(max_queries_per_batch)
+            for obj_name, result in results[stream].items():
+                all_results.setdefault(obj_name, []).extend(result)
 
-        return all_results
+            pbar.update(max_queries_per_batch)
+
+    return all_results
